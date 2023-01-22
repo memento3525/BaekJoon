2d69f99c9d6ade1c99a3ad55c39123f351c93f54
mentum <memento3525@nate.com>
2023-01-22 15:41:51 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.16 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int[] solution(int n) {
+        
+        int count = (n + 1) / 2;
+        int[] answer = new int[count];
+        
+        for(int i = 0; i < count; i ++)
+            answer[i] = i * 2 + 1;
+        
+        return answer;
+    }
+}
