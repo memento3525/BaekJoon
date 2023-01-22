a468ce1dc1983995259ce0c8e4c898e98c388749
mentum <memento3525@nate.com>
2023-01-22 14:54:55 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.46 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public string solution(string[] seoul) {
+        int index = Array.IndexOf(seoul, "Kim");
+        return $"김서방은 {index}에 있다";
+    }
+}
