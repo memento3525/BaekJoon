3269d927ff5877f4c2d1fe7dcf67b2b68795ecbb
mentum <memento3525@nate.com>
2023-01-23 20:55:53 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 64 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution
+{
+    static void Main()
+    {
+        string y = Console.ReadLine();
+
+        int year = int.Parse(y);
+        Console.WriteLine(year - 543);
+    }
+}
