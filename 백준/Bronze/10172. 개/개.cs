8211e953246532a67f4bbd34290652882b0441a8
mentum <memento3525@nate.com>
2023-01-23 21:32:26 +0900

    [Bronze V] Title: 개, Time: 60 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution
+{
+    static void Main()
+    {
+        Console.WriteLine("|\\_/|");
+        Console.WriteLine("|q p|   /}");
+        Console.WriteLine("( 0 )\"\"\"\\");
+        Console.WriteLine("|\"^\"`    |");
+        Console.WriteLine("||_/=\\\\__|");
+    }
+}
