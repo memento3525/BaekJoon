4bc49d07a82ffe68560794c02756b22fbdb5b28f
mentum <memento3525@nate.com>
2023-01-23 16:10:26 +0900

    [unrated] Title: 콜라 문제, Time: 0.17 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, int n) {
+        
+        int answer = 0;
+        
+        while(n >= a)
+        {
+            n -= a;
+            n += b;
+            answer += b;
+        }
+        
+        return answer;
+    }
+}
