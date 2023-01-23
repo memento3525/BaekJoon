7ec4320c2b8a6a7cd4c21536c80b1d9a71fe33ff
mentum <memento3525@nate.com>
2023-01-23 21:28:29 +0900

    [Bronze V] Title: 고양이, Time: 60 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution
+{
+    static void Main()
+    {
+        Console.WriteLine("\\    /\\");
+        Console.WriteLine(" )  ( ')");
+        Console.WriteLine("(  /  )");
+        Console.WriteLine(" \\(__)|");
+    }
+}
