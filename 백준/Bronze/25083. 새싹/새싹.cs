6e595e20bb8ba862dfa148122fda53c0f8cb5410
mentum <memento3525@nate.com>
2023-01-23 21:35:41 +0900

    [Bronze V] Title: 새싹, Time: 56 ms, Memory: 5312 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution
+{
+    static void Main()
+    {
+        Console.WriteLine("         ,r'\"7");
+        Console.WriteLine("r`-_   ,'  ,/");
+        Console.WriteLine(" \\. \". L_r'");
+        Console.WriteLine("   `~\\/");
+        Console.WriteLine("      |");
+        Console.WriteLine("      |");
+    }
+}
