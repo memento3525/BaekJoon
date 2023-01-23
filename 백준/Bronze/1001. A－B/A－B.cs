05fe306d8e8db75a27d401e1eb7c9dabd56d21e7
mentum <memento3525@nate.com>
2023-01-23 20:45:20 +0900

    [Bronze V] Title: A-B, Time: 76 ms, Memory: 5524 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        string[] strArray = s.Split();
+
+        int a = Convert.ToInt32(strArray[0]);
+        int b = Convert.ToInt32(strArray[1]);
+
+        Console.WriteLine(a - b);
+    }
+}
