19664bb0df7739643ace868e9e5a84bd148474bd
mentum <memento3525@nate.com>
2023-02-15 13:33:32 +0900

    [level 2] Title: 행렬의 곱셈, Time: 3.01 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/README.md
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/README.md
@@ -1,10 +1,10 @@
 # [level 2] 행렬의 곱셈 - 12949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949?language=csharp) 
 
 ### 성능 요약
 
-메모리: 31.6 MB, 시간: 0.76 ms
+메모리: 31.6 MB, 시간: 3.01 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.cs
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.cs
@@ -7,11 +7,11 @@ public class Solution {
         
         for(int a = 0; a < arr1.GetLength(0); a++)
         {
-            for(int b = 0; b < arr2.GetLength(1); b++)
+            for(int b = 0; b < arr1.GetLength(1); b++)
             {
-                for(int c = 0; c < arr2.GetLength(0); c++)
+                for(int c = 0; c < arr2.GetLength(1); c++)
                 {
-                    answer[a, b] += arr1[a, c] * arr2[c, b];
+                    answer[a, c] += arr1[a, b] * arr2[b, c];
                 }
             }
         }
