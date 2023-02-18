67e04e8551b0c3d54f7dfff5b6f5c06224c5a205
mentum <memento3525@nate.com>
2023-02-18 15:41:32 +0900

    [unrated] Title: 카드 뭉치, Time: 0.66 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.cs
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.cs
@@ -4,17 +4,15 @@ using System.Collections.Generic;
 public class Solution {
     public string solution(string[] cards1, string[] cards2, string[] goal) {
         
-        Array.Reverse(cards1);
-        Array.Reverse(cards2);
-        var stack1 = new Stack<string>(cards1);
-        var stack2 = new Stack<string>(cards2);
+        var stack1 = new Queue<string>(cards1);
+        var stack2 = new Queue<string>(cards2);
         
         for(int i = 0; i < goal.Length; i++)
         {
             if(stack1.Count > 0 && goal[i] == stack1.Peek())
-                stack1.Pop();
+                stack1.Dequeue();
             else if(stack2.Count > 0 && goal[i] == stack2.Peek())
-                stack2.Pop();
+                stack2.Dequeue();
             else
                 return "No";
         }
