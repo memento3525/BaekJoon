bd29f370f950accb49051f2f897be300a9e309b1
mentum <memento3525@nate.com>
2023-03-21 17:44:55 +0900

    [level 2] Title: 배달, Time: 1.77 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12978. 배달/배달.cs
+++ 프로그래머스/lv2/12978. 배달/배달.cs
@@ -18,53 +18,49 @@ class Solution // 다익스트라.
             int a = road[i, 0] - 1;
             int b = road[i, 1] - 1;
             int dist = road[i, 2];
-            
+
             if(dist < map[a, b])
                 map[a, b] = map[b, a] = dist;
         }
-        
+
         // 경로 찾기 전 초기화
         var times = new int[N];
         var visit = new bool[N];
         for(int i = 0; i < N; ++i)
             times[i] = map[0, i];
-        
+
         times[0] = 0; 
         visit[0] = true; 
-        
-        // 경로 찾기
-        while(true)
+
+        // 경로 찾기. 최초 시작위치에서 한개씩 확정짓기 때문에 시작노드를 제외한 N-1
+        for(int repeat = 0; repeat < N - 1; ++repeat)
         {
             int now = -1;
             int min = int.MaxValue;
-            // 최단찾기
+            // 아직 방문하지 않은 노드 중 최단거리 찾기
             for(int j = 0; j < N; ++j)
             {
                 if(visit[j]) continue;
                 if(times[j] == int.MaxValue) continue;
-                if(times[j] < min)
-                {
-                    min = times[j];
-                    now = j;
-                }
+                if(times[j] >= min) continue;
+                
+                min = times[j];
+                now = j;
             }
-            
-            if(now == -1)
-                break;
-            
+
             visit[now] = true;
-            
+
             for(int k = 0; k < N; ++k)
             {
                 if(visit[k]) continue;
                 if(map[now, k] == int.MaxValue) continue;
-                
+
                 // 직접 가는거보다 다른경로를 거쳐서 도달하는게 더 빠르면
                 if(times[k] > times[now] + map[now, k]) 
                     times[k] = times[now] + map[now, k];
             }
         }
-        
+
         return times.Count(c => c <= K);
     }
 }
