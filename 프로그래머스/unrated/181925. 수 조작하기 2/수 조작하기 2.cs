2883bade6dbcd6693b918086cc726b1091452f07
mentum <memento3525@nate.com>
2023-04-20 21:44:32 +0900

    [unrated] Title: 수 조작하기 2, Time: 1.49 ms, Memory: 32.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text;
+
+public class Solution {
+    public string solution(int[] numLog) 
+    {
+        var sb = new StringBuilder();
+        int n = numLog[0];
+        for(int i = 1; i < numLog.Length; ++i)
+        {
+            int next = numLog[i];
+            
+            if(n + 1 == next)       sb.Append("w");
+            else if(n - 1 == next)  sb.Append("s");
+            else if(n + 10 == next) sb.Append("d");
+            else if(n - 10 == next) sb.Append("a");
+            
+            n = next;
+        }
+        
+        return sb.ToString();
+    }
+}
