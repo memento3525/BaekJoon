1421925d161c7527e6bef4432b7ce83a0be23490
mentum <memento3525@nate.com>
2023-04-22 15:36:11 +0900

    [unrated] Title: 길이에 따른 연산, Time: 0.93 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public int solution(int[] num_list) 
+    {
+        if(num_list.Length >= 11)
+            return num_list.Sum();
+        else
+            return num_list.Aggregate((cur, next) => cur * next);
+    }
+}
