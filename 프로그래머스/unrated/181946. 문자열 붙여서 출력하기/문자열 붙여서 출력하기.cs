024a3784d15b6aa9de2de60bd004eaa6e9831a22
mentum <memento3525@nate.com>
2023-04-22 11:33:00 +0900

    [unrated] Title: 문자열 붙여서 출력하기, Time: 16.90 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        Console.Clear();
+        string str = Console.ReadLine().Replace(" ", string.Empty);
+        Console.WriteLine(str);
+    }
+}
