31a426a13c4df59f3690e4caf35a3904da553c47
mentum <memento3525@nate.com>
2023-04-23 16:59:18 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 9.60 ms, Memory: 31.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public int[] solution(int[] arr, int[,] queries) 
+    {
+        int[] answer = new int[queries.GetLength(0)];
+        for(int i = 0; i < queries.GetLength(0); ++i)
+        {
+            int s = queries[i, 0];
+            int e = queries[i, 1];
+            int k = queries[i, 2];
+            
+            int num = arr.Skip(s)
+                       .Take(e - s + 1)
+                       .Where(w => w > k)
+                       .DefaultIfEmpty(-1)
+                       .OrderBy(o => o)
+                       .First();
+            
+            answer[i] = num;
+        }
+        
+        return answer;
+    }
+}
