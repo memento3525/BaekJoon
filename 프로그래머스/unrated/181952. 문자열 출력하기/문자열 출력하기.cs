a8fd8b1eabb0a1cff20dc6eb0945af8c2014e190
mentum <memento3525@nate.com>
2023-04-23 13:29:36 +0900

    [unrated] Title: 문자열 출력하기, Time: 15.15 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        Console.Clear();
+        String s = Console.ReadLine();
+        Console.WriteLine(s);
+    }
+}
