ce428846934301c43c55f22978383ca2e74f555f
mentum <memento3525@nate.com>
2023-04-23 15:40:24 +0900

    [unrated] Title: 배열 조각하기, Time: 1.24 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        
+        int start = 0;
+        int end = arr.Length - 1;
+        for(int i = 0; i < query.Length; ++i)
+        {
+            if(i % 2 == 0)
+                end = start + query[i] - 1;
+            else
+                start = start + query[i];
+        }
+        
+        if(start > end) return new int[1]{ -1 };
+        return arr.Skip(start).Take(end - start + 1).ToArray();
+    }
+}
