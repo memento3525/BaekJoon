152d1290c77e800278e9c4429c47097f24157a39
mentum <memento3525@nate.com>
2023-04-25 22:00:16 +0900

    [unrated] Title: 수열과 구간 쿼리 1, Time: 0.31 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] arr, int[,] queries) 
+    {
+        for(int y = 0; y < queries.GetLength(0); ++y)
+        {
+            int s = queries[y, 0];
+            int e = queries[y, 1];
+            
+            for(int i = s; i <= e; ++i)
+                ++arr[i];
+        }
+        return arr;
+    }
+}
