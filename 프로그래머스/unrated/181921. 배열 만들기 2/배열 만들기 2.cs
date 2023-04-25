453d70b8799b0266c8fe7419861f0a6a55414139
mentum <memento3525@nate.com>
2023-04-25 21:53:26 +0900

    [unrated] Title: 배열 만들기 2, Time: 5.81 ms, Memory: 32.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public int[] solution(int l, int r) 
+    {
+        while(l % 5 != 0)
+            ++l;
+        
+        var list = new List<int>();
+        for(int i = l; i <= r; i += 5)
+        {
+            string str = i.ToString().Replace("0", "").Replace("5", "");
+            if(string.IsNullOrEmpty(str))
+                list.Add(i);
+        }
+        
+        return list.Count == 0 ? new int[1] { -1 } : list.ToArray();
+    }
+}
