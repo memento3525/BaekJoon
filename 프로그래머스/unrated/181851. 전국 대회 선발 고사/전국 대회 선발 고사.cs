92866cd36f2346961c7eac525a5b414ca2d31ed0
mentum <memento3525@nate.com>
2023-04-26 21:49:17 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 5.12 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public int solution(int[] rank, bool[] attendance) 
+    {
+        var list = new List<(int rank, int index)>();
+        for(int i = 0; i < rank.Length; ++i)
+        { 
+            if(!attendance[i]) continue;
+            list.Add((rank[i], i));
+        }
+        
+        var arr = list.OrderBy(o => o.Item1).Take(3).ToArray();
+        return 10000 * arr[0].Item2 + 100 * arr[1].Item2 + arr[2].Item2;
+    }
+}
