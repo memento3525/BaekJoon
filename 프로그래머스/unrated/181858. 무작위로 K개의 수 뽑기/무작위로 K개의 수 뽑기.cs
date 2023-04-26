a194379f82f858a300e7462f6ff47c4d1cd36953
mentum <memento3525@nate.com>
2023-04-26 21:43:41 +0900

    [unrated] Title: 무작위로 K개의 수 뽑기, Time: 3.60 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public int[] solution(int[] arr, int k) 
+    {
+        int[] distinct = arr.Distinct().Take(k).ToArray();
+        int[] answer = Enumerable.Repeat(-1, k).ToArray();
+        Array.Copy(distinct, 0, answer, 0, distinct.Length);
+        
+        return answer;
+    }
+}
