bf897c5439fa508f3c7731ac9becb47e62925828
mentum <memento3525@nate.com>
2023-05-14 10:49:37 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 4.39 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public int solution(string s) 
+    {
+        int answer = 1;
+        
+        for(int i = 1; i < s.Length - 1; ++i)
+        {
+            // 홀수 구성 체크 aba
+            Check(s, 1, i, i, ref answer);
+            
+            // 짝수 구성 체크 abba
+            if(s[i] == s[i + 1])
+                Check(s, 2, i, i + 1, ref answer);
+        }
+        
+        return answer;
+    }
+    
+    void Check(string s, int startCount, int left, int right, ref int answer)
+    {            
+        int count = startCount;
+            
+        while(true)
+        {
+            if(--left < 0) break;
+            if(++right >= s.Length) break;
+            if(s[left] != s[right]) break;
+            count += 2;
+        }
+
+        if(count > answer)
+            answer = count;
+    }
+}
