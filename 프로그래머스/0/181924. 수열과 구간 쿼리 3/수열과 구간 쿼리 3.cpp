d00aa074147767d6569d2c2acad8c95979d4bde1
mentum <memento3525@nate.com>
2024-11-14 13:04:39 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.23 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.cpp
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.cpp
@@ -6,11 +6,7 @@ using namespace std;
 vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
     
     for (const vector<int> v : queries)
-    {
-        int temp = arr[v[0]];
-        arr[v[0]] = arr[v[1]];
-        arr[v[1]] = temp;
-    }
+        swap(arr[v[0]], arr[v[1]]);
     
     return arr;
 }
