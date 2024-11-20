88f80e5847d8c1a398955dbb32ba0bf3d1778dcf
mentum <memento3525@nate.com>
2024-11-20 10:54:40 +0900

    [level 0] Title: 리스트 자르기, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, vector<int> slicer, vector<int> num_list) {
+    vector<int> answer;
+
+    int start = (n == 1) ? 0 : slicer[0];
+    int end = (n == 2) ? num_list.size() - 1 : slicer[1];
+    int interval = (n == 4) ? slicer[2] : 1;
+    
+    for (int i = start; i <= end; i += interval)
+    {
+        answer.push_back(num_list[i]);
+    }
+    
+    return answer;
+}
