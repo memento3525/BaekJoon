405d4937f21e648a89263484848bb19fd1770d91
mentum <memento3525@nate.com>
2024-12-01 13:54:47 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(string my_string) {
+    vector<int> answer;
+    
+    for (char c : my_string)
+    {
+        if (std::isdigit(c))
+            answer.push_back(c - '0');
+    }
+    
+    std::sort(answer.begin(), answer.end());
+    return answer;
+}
