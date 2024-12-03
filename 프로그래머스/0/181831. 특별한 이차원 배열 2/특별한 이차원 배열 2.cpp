ad1e8581606c0655ce0f4b3fd5c36c3450bbdb64
mentum <memento3525@nate.com>
2024-12-03 11:43:28 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.05 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.cpp
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.cpp
@@ -7,7 +7,7 @@ int solution(vector<vector<int>> arr) {
     
     for (int y = 0; y < arr.size(); ++y)
     {
-        for (int x = 0; x < arr[y].size(); ++x)
+        for (int x = 0; x < y; ++x)
         {
             if (arr[y][x] != arr[x][y])
                 return 0;
