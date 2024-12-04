a2eb4b3aa147c676f8150a1483e6503341e8e9c3
mentum <memento3525@nate.com>
2024-12-04 12:02:17 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    while (true)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (a == 0 && b == 0)
+            break;
+        
+        cout << a + b << '\n';
+    }
+}
