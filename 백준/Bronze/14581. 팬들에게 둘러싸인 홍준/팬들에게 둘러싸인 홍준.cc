0abb7dadc9a22d0a9da083bb5561b97cae918a09
mentum <memento3525@nate.com>
2024-12-07 14:57:42 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    string s;
+    cin >> s;
+
+    cout << ":fan::fan::fan:" << '\n';
+    cout << ":fan::" << s << "::fan:" << '\n';
+    cout << ":fan::fan::fan:" << '\n';
+}
