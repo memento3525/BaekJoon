9a11acae45f10155342eb640db0b17de9745c5f9
mentum <memento3525@nate.com>
2024-12-08 15:26:53 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 4516 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+
+    int arr[10001]{};
+
+    int temp;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> temp;
+        ++arr[temp];
+    }
+
+    for (int i = 1; i <= 10000; ++i)
+    {
+        if (arr[i] == 0) continue;
+
+        for (int k = 0; k < arr[i]; ++k)
+            cout << i << '\n';
+    }
+}
