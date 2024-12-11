aafe50699bf8293749657255e26afa9e854e391b
mentum <memento3525@nate.com>
2024-12-11 15:23:52 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -2,60 +2,52 @@
 #include <vector>
 using namespace std;
 
-void Combi(vector<int> v, int cur);
-
-int n;
-int pickSize = 3;
-int maxValue;
-vector<int> cards;
-int result = 0;
-
 int main(void)
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
+    int n, maxValue;
+
     cin >> n >> maxValue;
 
-    cards = vector<int>(n);
+    vector<int> cards(n);
 
+    int temp;
     for (int i = 0; i < n; ++i)
     {
-        int temp;
         cin >> temp;
         cards[i] = temp;
     }
 
-    vector<int> v;
-    Combi(v, -1);
+    int result = 0;
+    for (int a = 0; a < n; ++a)
+    {
+        int sum1 = cards[a];
+        if (sum1 > maxValue) continue;
+
+        for (int b = 0; b < n; ++b)
+        {
+            if (a == b) continue;
+
+            int sum2 = sum1 + cards[b];
+            if (sum2 > maxValue) continue;
+
+            for (int c = 0; c < n; ++c)
+            {
+                if (a == b) continue;
+                if (a == c) continue;
+                if (b == c) continue;
+
+                int sum3 = sum2 + cards[c];
+                if (sum3 > maxValue) continue;
+
+                if (result < sum3)
+                    result = sum3;
+            }
+        }
+    }
 
     cout << result;
-}
-
-void Combi(vector<int> v, int start)
-{
-    if (v.size() == pickSize)
-    {
-        int sum = 0;
-        for (const int& index : v)
-        {
-            sum += cards[index];
-        }
-
-        if (sum > maxValue)
-            return;
-
-        if (result < sum)
-            result = sum;
-
-        return;
-    }
-
-    for (int i = start + 1; i < n; ++i)
-    {
-        v.push_back(i);
-        Combi(v, i);
-        v.pop_back();
-    }
 }
