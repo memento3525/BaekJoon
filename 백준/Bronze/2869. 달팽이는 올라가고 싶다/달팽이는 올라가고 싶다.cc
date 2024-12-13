0350abddedf6dddc651555b4e0baf46f1918d022
mentum <memento3525@nate.com>
2024-12-13 12:42:46 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -3,39 +3,15 @@ using namespace std;
 
 int main(void)
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
     int a, b, v;
     cin >> a >> b >> v;
 
-    int day = 0;
-    int remain = v;
+    int target = v - a;
     int interval = a - b;
 
-    while (remain >= a)
-    {
-        int div = remain / a;
+    int day = target / interval + 1;
 
-        if (div == 1 && remain % a == 0) // 마지막 스탭
-        {
-            ++day;
-            remain = 0;
-            break;
-        }
-        else if (div > 0)
-        {
-            day += div;
-            remain -= div * interval;
-        }
-        else
-        {
-            break;
-        }
-    }
-
-    if (remain > 0)
+    if (target % interval != 0)
         ++day;
 
     cout << day;
