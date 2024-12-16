f28b14bd15a8a23387ec5b18638a6d3f47c1c091
mentum <memento3525@nate.com>
2024-12-16 11:19:39 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+
+    int temp;
+    while (cin >> temp)
+    {
+        cout << temp << ' ' << temp << '\n';
+    }
+}
