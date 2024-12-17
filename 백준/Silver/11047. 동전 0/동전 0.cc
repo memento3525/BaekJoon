aca10f249e6727634f6f23275b1a44a2378de1fa
mentum <memento3525@nate.com>
2024-12-17 12:45:42 +0900

    [Silver IV] Title: 동전 0, Time: 60 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, k;
+    cin >> n >> k;
+
+    int temp;
+    vector<int> v;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> temp;
+
+        if (temp <= k)
+            v.emplace_back(temp);
+    }
+
+    int remain = k;
+    int count = 0;
+    vector<int> coins;
+    for (int i = v.size() - 1; i >= 0; --i)
+    {
+        while (v[i] <= remain)
+        {
+            ++count;
+            remain -= v[i];
+
+            if (remain == 0)
+                break;
+        }
+    }
+
+    cout << count;
+}
