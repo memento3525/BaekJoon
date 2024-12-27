be8c4f131da95910f360243dd79e97c2da048739
mentum <memento3525@nate.com>
2024-12-27 12:57:04 +0900

    [Gold III] Title: 사다리 조작, Time: 1204 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.cc
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> visited;
+int width, m, height;
+int ret = INT32_MAX;
+
+bool IsValid()
+{
+    for (int x = 1; x <= width; ++x)
+    {
+        int current = x;
+        for (int y = 1; y <= height; ++y)
+        {
+            if (visited[y][current])
+                ++current;
+            else if (visited[y][current - 1])
+                --current;
+        }
+
+        if (current != x)
+            return false;
+    }
+    return true;
+}
+
+void Combi(int here, int cnt)
+{
+    if (cnt > 3 || cnt >= ret) return;
+
+    if (IsValid())
+    {
+        if (cnt < ret)
+            ret = cnt;
+        return;
+    }
+
+    for (int y = here; y <= height; ++y)
+    {
+        for (int x = 1; x <= width; ++x)
+        {
+            if (visited[y][x]) continue;
+            if (visited[y][x - 1] || visited[y][x + 1]) continue;
+            
+            visited[y][x] = 1;
+            Combi(y, cnt + 1);
+            visited[y][x] = 0;
+        }
+    }
+}
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> width >> m >> height;
+
+    visited.resize(height + 1);
+    for (int i = 0; i <= height; ++i)
+        visited[i].resize(width + 2); // 좌우 범위검사 제외용
+
+    int a, b;
+    for (int i = 0; i < m; ++i)
+    {
+        cin >> a >> b;
+        visited[a][b] = 1;
+    }
+
+    Combi(1, 0);
+
+    cout << ((ret == INT32_MAX) ? -1 : ret);
+}
