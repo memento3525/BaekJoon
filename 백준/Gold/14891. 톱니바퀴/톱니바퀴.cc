e24a036e8ef1a500feab3cdd0d93ff61bc6359f8
mentum <memento3525@nate.com>
2025-01-08 14:20:15 +0900

    [Gold V] Title: 톱니바퀴, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

---
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+vector<string> gear;
+vector<int> offset;
+
+char _getL(int index) 
+{ 
+    int i = (6 + offset[index]) % 8;
+    return gear[index][i];
+}
+char _getR(int index) 
+{
+    int i = (2 + offset[index]) % 8;
+    return gear[index][i];
+}
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t = 4;
+    int k, index, dir;
+
+    gear.resize(t);
+    for (int i = 0; i < t; ++i)
+        cin >> gear[i];
+
+    offset.resize(t);
+
+    cin >> k;
+    vector<int> moving(t);
+    for (int i = 0; i < k; ++i)
+    {
+        cin >> index >> dir;
+        --index;
+
+        moving[index] = dir;
+
+        int left = index;
+        int leftDir = -dir;
+        while (left - 1 >= 0)
+        {
+            if (_getR(left - 1) == _getL(left))
+                break;
+
+            moving[left - 1] += leftDir;
+            --left;
+            leftDir = -leftDir;
+        }
+
+        int right = index;
+        int rightDir = -dir;
+        while (right + 1 < t)
+        {
+            if (_getR(right) == _getL(right + 1))
+                break;
+
+            moving[right + 1] += rightDir;
+            ++right;
+            rightDir = -rightDir;
+        }
+
+        // 회전 실제 적용
+        for (int i = 0; i < t; ++i)
+        {
+            offset[i] = (offset[i] - moving[i] + 8) % 8;
+            moving[i] = 0;
+        }
+    }
+
+    int ret = 0;
+    for (int i = 0; i < t; ++i)
+    {
+        if (gear[i][offset[i] % 8] == '1')
+            ret += pow(2, i);
+    }
+
+    cout << ret;
+}
