6a62efed4ac4f84aa8a83280a6130a9c74abe778
mentum <memento3525@nate.com>
2025-01-11 14:38:45 +0900

    [Gold III] Title: 감시, Time: 8 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.cc
+++ 백준/Gold/15683. 감시/감시.cc
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 int dy[4]{-1, 0, 1, 0 };
-int dx[4]{ 0, 1, 0,-1 }; // 북 동 남 서
+int dx[4]{ 0, 1, 0,-1 };
 
 vector<vector<int>> map;
 int height, width;
@@ -16,14 +16,12 @@ struct Cam
     Cam(int _t, int _y, int _x) : y(_y), x(_x), t(_t) {}
 };
 
-vector<vector<pair<int, int>>> cameras; // 카메라 인덱스별 분류
 vector<Cam> cams;
-vector<int> camDir;
-int camIdx;
+vector<pair<int, int>> changed;
 
 int ret = INT32_MAX;
 
-void Ray(vector<vector<int>>& _map, int y, int x, int dir)
+void Ray(int y, int x, int dir)
 {
     while (true)
     {
@@ -31,14 +29,17 @@ void Ray(vector<vector<int>>& _map, int y, int x, int dir)
         x += dx[dir];
 
         if (y < 0 || y >= height || x < 0 || x >= width) break;
-        if (_map[y][x] == 6) break;
+        if (map[y][x] == 6) break;
 
-        if (_map[y][x] == 0)
-            _map[y][x] = 7;
+        if (map[y][x] == 0)
+        {
+            map[y][x] = 7;
+            changed.push_back({ y, x });
+        }
     }
 }
 
-void Go(vector<vector<int>> _map, int ci)
+void Go(int ci)
 {
     if (ci == cams.size())
     {
@@ -47,7 +48,7 @@ void Go(vector<vector<int>> _map, int ci)
         {
             for (int x = 0; x < width; ++x)
             {
-                if (_map[y][x] == 0)
+                if (map[y][x] == 0)
                     ++sum;
             }
         }
@@ -60,49 +61,50 @@ void Go(vector<vector<int>> _map, int ci)
 
     for (int i = 0; i < 4; ++i)
     {
-        camDir[ci] = i;
-
         int type = cams[ci].t;
         int y = cams[ci].y;
         int x = cams[ci].x;
 
-        vector<vector<int>> _origin = _map;
+        changed.clear();
 
         if (type == 1)
         {
-            Ray(_map, y, x, i);
+            Ray(y, x, i);
         }
         else if (type == 2)
         {
-            Ray(_map, y, x, i);
-            Ray(_map, y, x, i + 2);
+            Ray(y, x, i);
+            Ray(y, x, i + 2);
         }
         else if (type == 3)
         {
-            Ray(_map, y, x, i);
-            Ray(_map, y, x, (i + 1) % 4);
+            Ray(y, x, i);
+            Ray(y, x, (i + 1) % 4);
         }
         else if (type == 4)
         {
-            Ray(_map, y, x, (i + 3) % 4);
-            Ray(_map, y, x, i);
-            Ray(_map, y, x, (i + 1) % 4);
+            Ray(y, x, (i + 3) % 4);
+            Ray(y, x, i);
+            Ray(y, x, (i + 1) % 4);
         }
         else if (type == 5)
         {
-            Ray(_map, y, x, i);
-            Ray(_map, y, x, i + 1);
-            Ray(_map, y, x, i + 2);
-            Ray(_map, y, x, i + 3);
+            Ray(y, x, i);
+            Ray(y, x, i + 1);
+            Ray(y, x, i + 2);
+            Ray(y, x, i + 3);
         }
 
-        Go(_map, ci + 1);
+        vector<pair<int, int>> cachedChanged = changed;
 
-        if (type == 5) break;// 5번캠은 1번만
-        if (type == 2 && i == 1) break; // 2번 캠은 2번만
+        Go(ci + 1);
 
         // 복구
-        _map = _origin;
+        for (const auto& pair : cachedChanged)
+            map[pair.first][pair.second] = 0;
+
+        if (type == 5) break; // 5번 캠은 1번만
+        if (type == 2 && i == 1) break; // 2번 캠은 2번만
     }
 }
 
@@ -115,7 +117,6 @@ int main(void)
     cin >> height >> width;
 
     cams.reserve(8);
-    cameras.resize(6);
     map.assign(height, vector<int>(width));
     int temp;
     for (int y = 0; y < height; ++y)
@@ -126,27 +127,11 @@ int main(void)
             map[y][x] = temp;
 
             if (temp >= 1 && temp <= 5)
-            {
-                cameras[temp].push_back({ y, x });
                 cams.push_back(Cam(temp, y, x));
-            }
         }
     }
 
-    camDir.resize(cams.size());
-
-    // 5번 카메라는 방향이 없으니 무조건 진행
-    for (int i = 0; i < cameras[5].size(); ++i)
-    {
-        int y = cameras[5][i].first;
-        int x = cameras[5][i].second;
-
-        for (int d = 0; d < 4; ++d)
-            Ray(map, y, x, d);
-    }
-
-    Go(map, 0);
+    Go(0);
 
     cout << ret;
-    return 0;
 }
