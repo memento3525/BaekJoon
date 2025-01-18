82e6327d8f10a0a88f7efcd6b865d78a33db35d2
mentum <memento3525@nate.com>
2025-01-18 21:30:02 +0900

    [Gold I] Title: 외판원 순회, Time: 28 ms, Memory: 6116 KB -BaekjoonHub

---
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+const int MAX_N = 16;
+
+int n;
+int dp[MAX_N][1 << MAX_N];
+int cost[MAX_N][MAX_N];
+int INF = 999999999;
+
+int Go(int here, int visited)
+{
+    if (visited == (1 << n) - 1) // 전부 방문
+    {
+        return cost[here][0] ? cost[here][0] : INF;
+    }
+
+    int &ret = dp[here][visited];
+    if (ret != -1) return ret;
+
+    ret = INF;
+    for (int i = 0; i < n; ++i)
+    {
+        if (visited & (1 << i)) continue;
+        if (cost[here][i] == 0) continue;
+
+        ret = min(ret, Go(i, visited | (1 << i)) + cost[here][i]);
+    }
+
+    return ret;
+}
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n;
+
+    for (int y = 0; y < n; ++y)
+    {
+        for (int x = 0; x < n; ++x)
+            cin >> cost[y][x];
+    }
+
+    memset(dp, -1, sizeof(dp));
+
+    // 시작점이 0이라고 가정하고 시작.
+    // 외판원 문제는 어느위치에서 시작하던 순회 최소비용이라 결과가 동일함.
+    cout << Go(0, 1);
+}
