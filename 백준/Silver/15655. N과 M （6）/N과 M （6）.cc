8e95d379ce4a15993595038f27a6a1a0371cc917
mentum <memento3525@nate.com>
2025-01-20 15:40:36 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+vector<int> nums;
+vector<int> ret;
+vector<int> visited;
+
+void Dfs(int depth, int prev)
+{
+    if (depth == m)
+    {
+        for (int i : ret)
+            cout << nums[i] << ' ';
+
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (visited[i]) continue;
+        if (prev != -1 && nums[prev] > nums[i]) continue;
+
+        visited[i] = 1;
+        ret[depth] = i;
+        Dfs(depth + 1, i);
+        visited[i] = 0;
+    }
+}
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+
+    nums.resize(n);
+    visited.resize(n);
+    ret.resize(m);
+
+    for (int i = 0; i < n; ++i)
+        cin >> nums[i];
+
+    sort(nums.begin(), nums.end());
+
+    Dfs(0, -1);
+}
