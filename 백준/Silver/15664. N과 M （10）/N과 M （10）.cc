db7f52e2064e65707229800f40caa2a4238ceb6e
mentum <memento3525@nate.com>
2025-01-21 10:17:49 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2084 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+#include <sstream>
+using namespace std;
+
+int n, m;
+vector<int> nums;
+vector<int> ret;
+vector<int> visited;
+unordered_set<string> retSet;
+stringstream ss;
+
+void Dfs(int depth, int prev)
+{
+    if (depth == m)
+    {
+        ss.str("");
+        ss.clear();
+
+        for (int i : ret)
+            ss << nums[i] << ' ';
+
+        const string& output = ss.str();
+
+        if (retSet.find(output) == retSet.end())
+        {
+            cout << output << '\n';
+            retSet.insert(output);
+        }
+        return;
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (prev != -1 && nums[prev] > nums[i]) continue;
+        if (visited[i]) continue;
+
+        visited[i] = 1;
+        ret[depth] = i;
+        Dfs(depth + 1, i);
+        visited[i] = 0;
+    }
+}
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+
+    nums.resize(n);
+    visited.resize(n);
+    ret.resize(m);
+
+    for (int i = 0; i < n; ++i)
+        cin >> nums[i];
+
+    sort(nums.begin(), nums.end());
+
+    Dfs(0, -1);
+}
