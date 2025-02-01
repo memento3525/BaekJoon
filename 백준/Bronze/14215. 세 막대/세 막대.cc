4d26eb1b06b47028b3ae8a4c63b2e9a6a5997c63
mentum <memento3525@nate.com>
2025-02-01 15:03:42 +0900

    [Bronze III] Title: 세 막대, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/14215. 세 막대/세 막대.cc
+++ 백준/Bronze/14215. 세 막대/세 막대.cc
@@ -1,21 +1,8 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-using namespace std;
-
-int main(void)
-{
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
-    vector<int> v(3);
-    cin >> v[0] >> v[1] >> v[2];
-    
-    sort(v.begin(), v.end());
-
-    if (v[2] >= v[0] + v[1])
-        v[2] = v[0] + v[1] - 1;
-
-    cout << (v[0] + v[1] + v[2]);
+int main(void){
+    int v[3];
+    std::cin >> v[0] >> v[1] >> v[2];
+    std::sort(v, v + 3);
+    std::cout << (v[0] + v[1] + std::min(v[2], v[0] + v[1] - 1));
 }
