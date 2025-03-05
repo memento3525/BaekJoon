1198e1106f96cd368d9b32cea143bc50315decbc
mentum <memento3525@gmail.com>
2025-03-05 11:31:34 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 68 ms, Memory: 10080 KB -BaekjoonHub

--- 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.cc
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.cc
@@ -2,10 +2,8 @@
 #include <set>
 using namespace std;
 
-int n, person, ret, curPerson;
-char game;
+int n, person, ret;
 set<string> s;
-string name;
 
 int main(void)
 {
@@ -13,28 +11,20 @@ int main(void)
     cin.tie(0);
     cout.tie(0);
 
+    char game;
     cin >> n >> game;
 
     if (game == 'Y') person = 1;
     else if (game == 'F') person = 2;
     else person = 3;
 
+    string name;
     while (n--)
     {
         cin >> name;
-
-        if (s.find(name) != s.end())
-            continue;
-        
         s.insert(name);
-        ++curPerson;
-
-        if (curPerson == person)
-        {
-            ++ret;
-            curPerson = 0;
-        }
     }
 
+    int ret = (s.size() / person);
     cout << ret;
 }
